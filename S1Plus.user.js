8476f9bc77f4dbcfe7741b1b971e6bfd6b0800f3
moekyo <shigotods@outlook.com>
2025-08-05 23:34:07 +0800

    release: v4.1.0

--- S1Plus.user.js
+++ S1Plus.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         S1 Plus - Stage1st 体验增强套件
 // @namespace    http://tampermonkey.net/
-// @version      4.0.2
+// @version      4.1.0
 // @description  为Stage1st论坛提供帖子/用户屏蔽、导航栏自定义、自动签到、阅读进度跟踪等多种功能，全方位优化你的论坛体验。
 // @author       moekyo
 // @match        https://stage1st.com/2b/*
@@ -14,7 +14,7 @@
 (function () {
     'use strict';
 
-    const SCRIPT_VERSION = '4.0.2';
+    const SCRIPT_VERSION = '4.1.0';
     const SCRIPT_RELEASE_DATE = '2025-08-05';
 
     // --- 样式注入 ---
